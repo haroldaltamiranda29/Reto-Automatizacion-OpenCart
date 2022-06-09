562def7adc5badf7809813122803223320a2d041
Harold Altamiranda <harold29as@gmail.com>
2022-06-08 21:42:02 -0500

    Actualizacion proyecto

--- src/test/java/co/com/choucair/opencart/stepdefinitions/OpenCartStepDefinition.java
+++ src/test/java/co/com/choucair/opencart/stepdefinitions/OpenCartStepDefinition.java
@@ -3,15 +3,19 @@ package co.com.choucair.opencart.stepdefinitions;
 import co.com.choucair.opencart.models.CustomersOpenCart;
 import co.com.choucair.opencart.models.FiltroOpenCart;
 import co.com.choucair.opencart.models.LoginOpenCart;
+import co.com.choucair.opencart.models.ProductoOpenCart;
+import co.com.choucair.opencart.questions.Pedido;
+import co.com.choucair.opencart.questions.Validar;
+import co.com.choucair.opencart.questions.ValidarPedido;
 import co.com.choucair.opencart.tasks.*;
 import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
 import cucumber.api.java.es.Y;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-
 import java.util.List;
 
 public class OpenCartStepDefinition {
@@ -37,7 +41,8 @@ public class OpenCartStepDefinition {
     }
 
     @Entonces("^deberia visualizar el siguiente mensaje \"([^\"]*)\"$")
-    public void deberiaVisualizarElSiguienteMensaje(String arg1) {
+    public void deberiaVisualizarElSiguienteMensaje(String mensaje) {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Validar.mensaje(mensaje)));
     }
 
     @Y("^selecciono la opcion pedidos de la categoria ventas$")
@@ -52,17 +57,21 @@ public class OpenCartStepDefinition {
 
     @Entonces("^deberia visualizar el pedido con orderID \"([^\"]*)\" store \"([^\"]*)\" y customer \"([^\"]*)\"$")
     public void deberiaVisualizarElPedidoConOrderIDStoreYCustomer(String texto1, String texto2, String texto3) {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Pedido.deLaOrden(texto1,texto2,texto3)));
     }
 
     @Y("^selecciono ver una orden de los ultimos pedidos$")
     public void seleccionoVerUnaOrdenDeLosUltimosPedidos() {
+        OnStage.theActorInTheSpotlight().attemptsTo(Ver.orden());
     }
 
     @Cuando("^agrego un producto a la orden con su informacion$")
-    public void agregoUnProductoALaOrdenConSuInformacion() {
+    public void agregoUnProductoALaOrdenConSuInformacion(List<ProductoOpenCart> productoOpenCarts) {
+        OnStage.theActorInTheSpotlight().attemptsTo(AgregarProducto.aLaOrden(productoOpenCarts));
     }
 
     @Entonces("^se valida que se modifique el carito de compra con el siguiente mensaje \"([^\"]*)\"$")
-    public void seValidaQueSeModifiqueElCaritoDeCompraConElSiguienteMensaje(String arg1) {
+    public void seValidaQueSeModifiqueElCaritoDeCompraConElSiguienteMensaje(String mensaje) {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(ValidarPedido.deLaOrden(mensaje)));
     }
 }
